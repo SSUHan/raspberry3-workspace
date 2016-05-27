da4e980a71682d11d02f085c5d0dbf7538de936e
BrianLee <ljs93kr@gmail.com>
2016-05-27 15:51:14 +0000

    keyboard controll complete

--- servo_moter/servo_kbctl.py
+++ servo_moter/servo_kbctl.py
@@ -5,32 +5,31 @@ pin = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
 
-p = GPIO.PWM(pin, 50)
+p = GPIO.PWM(pin, 50) # 50 Hz
 p.start(0)
+# this values validate only when 50Hz 
+left_angle = 12.5
+center_angle = 7.5
+right_angle = 2.5
+
 def doAngle(angle):
     p.ChangeDutyCycle(angle)
     print "Angle: %d" % angle
     time.sleep(0.5)
 
-
-
-
-var = 1
 try:
 
     while True:
-        var = float(raw_input("dutyCycle value : "))
-        doAngle(var)
-#        var = raw_input("Enter L/R/C : ")
-#        if var == 'R' or var == 'r' :
-#            print "Right"
-#            doAngle(2.5)
-#        elif var == 'L' or var == 'l':
-#            print "Left"
-#            doAngle(12.5)
-#        elif var == 'C' or var == 'c':
-#            print "Center"
-#            doAngle(7.5)
+        var = raw_input("Enter L/R/C : ")
+        if var == 'R' or var == 'r' :
+            print "Right"
+            doAngle(right_angle)
+        elif var == 'L' or var == 'l':
+            print "Left"
+            doAngle(left_angle)
+        elif var == 'C' or var == 'c':
+            print "Center"
+            doAngle(center_angle)
 except KeyboardInterrupt:
     p.stop()
 GPIO.cleanup()
